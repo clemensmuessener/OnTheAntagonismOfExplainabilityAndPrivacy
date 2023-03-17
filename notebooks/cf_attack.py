dcd801030a3a2dcaed16c123331c237b52c35593
Martin Lange <ml_ks@web.de>
2023-03-17 14:18:47 +0100

    Add new results for 1.6

--- notebooks/cf_attack.py
+++ notebooks/cf_attack.py
@@ -1,4 +1,4 @@
-from experiment_setup import TrainingDataExtraction
+from experiment_setup import MembershipInference, TrainingDataExtraction
 import dice_ml
 import numpy as np
 import random
@@ -6,6 +6,76 @@ import pandas as pd
 import logging
 
 logger = logging.getLogger('xai-privacy')
+
+
+class CounterfactualMembershipInference(MembershipInference):
+    def train_explainer(self, data_train, model):
+        # train explainer on training data
+        d = dice_ml.Data(dataframe=data_train, continuous_features=self.numeric_features, \
+                         outcome_name=self.outcome_name)
+        m = dice_ml.Model(model=model, backend="sklearn", model_type='classifier')
+
+        # use method "kd-tree" to get counterfactuals drawn from the training data
+        return dice_ml.Dice(d, m, method="kdtree")
+
+    @staticmethod
+    def membership_inference_attack_no_model_access(explainer, samples_df):
+        num_samples = len(samples_df)
+        inferred_membership = np.full(num_samples, False)
+        # we only use the features for membership inference, not the target. Therefore we must drop the last column.
+        samples_df = samples_df.drop(samples_df.columns[-1], axis=1)
+
+        # This is the default number of counterfactuals per query used on the github page of DiCE
+        cfs_per_query = 4
+
+        # get first counterfactuals for all given samples
+        e1 = explainer.generate_counterfactuals(samples_df, total_CFs=cfs_per_query, desired_class='opposite')
+
+        # collect all first counterfactuals in this dataframe to plug it into the explainer once more
+        first_cfs_all = pd.DataFrame(columns=samples_df.columns)
+
+        # collect the original sample index corresponding to a first counterfactual
+        # this is necessary in order to remember which first counterfactuals belonged to which original sample
+        respective_sample_index = []
+
+        # collect first counterfactuals
+        for index in range(num_samples):
+            # get counterfactuals for given sample:
+            first_cfs = e1.cf_examples_list[index].final_cfs_df
+            logger.debug(f'Sample {index}: 1st counterfactuals: \n {first_cfs.to_numpy()}')
+
+            first_cfs_all = pd.concat([first_cfs_all, first_cfs])
+
+            for i in range(len(first_cfs)):
+                respective_sample_index.append(index)
+
+        respective_sample_index = np.array(respective_sample_index)
+
+        # get second counterfactuals for all first counterfactuals
+        e2 = explainer.generate_counterfactuals(first_cfs_all, total_CFs=cfs_per_query, desired_class='opposite')
+
+        # compare all second counterfactuals with the samples they were generated for
+        for i, second_cfs_obj in enumerate(e2.cf_examples_list):
+            # get the sample that these second counterfactuals belong to:
+            index = respective_sample_index[i]
+            sample_df = samples_df.iloc[[index], :]
+
+            logger.debug(f'Sample {index}: {sample_df.to_numpy()}')
+
+            second_cfs = second_cfs_obj.final_cfs_df
+
+            logger.debug(f'Sample {index}: 2nd counterfactuals: \n {second_cfs.to_numpy()}')
+
+            # if any counter-counterfactual is equal to the given sample, then it is part of the training data:
+            # np.isclose is used for comparison because explainer may round floating point values
+            result = np.isclose(second_cfs.to_numpy().astype(float), sample_df.to_numpy().astype(float)).all(
+                axis=1).any()
+
+            if result:
+                logger.debug(f'Inferred membership as true.')
+                inferred_membership[index] = True
+
+        return inferred_membership
 
 
 class CounterfactualTDE(TrainingDataExtraction):
